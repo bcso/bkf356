eeb855cfb4c46e31eb0f1efd4ab97a05efa58500
Krishn Ramesh <krishnr@gmail.com>
2017-12-01 15:03:11 -0500

    regression and classification working

--- classification/classification.py
+++ classification/classification.py
@@ -1,7 +1,6 @@
 import os
 import pymysql.cursors
 from pprint import pprint
-import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import train_test_split
@@ -11,6 +10,8 @@ from sklearn.metrics import accuracy_score
 
 from sklearn.externals.six import StringIO  
 # import pydot 
+
+from sklearn.ensemble import RandomForestClassifier
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -25,21 +26,35 @@ user_id = 'ELcQDlf69kb-ihJfxZyL0A'
 
 try:
     with connection.cursor() as cursor:
-        sql = "select b.id b_id, b.neighborhood b_hood, b.name b_name, b.address b_address, b.city b_city, b.state b_state, b.postal_code b_pc, b.latitude b_lat, b.longitude b_longi, b.stars b_stars, b.review_count b_review_count, r.stars r_stars from business b join review r on r.business_id = b.id where r.user_id = %s"
+        sql = "select c.category c_category, b.id b_id, b.neighborhood b_hood, b.name b_name, b.address b_address, b.city b_city, b.state b_state, b.postal_code b_pc, b.latitude b_lat, b.longitude b_longi, b.stars b_stars, b.review_count b_review_count, r.stars r_stars from business b join review r on r.business_id = b.id join category c on c.business_id = b.id where r.user_id = %s"
         cursor.execute(sql, [user_id])
         reviews = cursor.fetchall()
 finally:
     connection.close()
 
+y = []
+X = []
+for review in reviews:
+    row = {
+        'b_review_count': review['b_review_count'],
+        'b_hood': review['b_hood'],
+        'b_name': review['b_name'],
+        'b_address': review['b_address'],
+        'b_city': review['b_city'],
+        'b_state': review['b_state'],
+        'b_stars': review['b_stars'],
+        # 'b_postal_code': review['b_pc'],
+        'c_category': review['c_category'],
+    }
+    X.append(row)
+    y.append(review['r_stars'])
+
 vec = DictVectorizer()
-vectorized_features = vec.fit_transform(reviews).toarray()
+vectorized_features = vec.fit_transform(X).toarray()
 feature_names = vec.get_feature_names()
 
-df1 = pd.DataFrame(reviews)
-
-y = df1['r_stars']
 X_train, X_test, y_train, y_test = train_test_split(vectorized_features, y, test_size=0.5, random_state=42)
-dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+dt = DecisionTreeClassifier(min_samples_split=100, random_state=99)
 dt = dt.fit(X_train, y_train)
 
 dot_data = export_graphviz(dt, out_file='test.dot', 
@@ -49,4 +64,10 @@ dot_data = export_graphviz(dt, out_file='test.dot',
                          special_characters=True) 
 
 y_pred = dt.predict(X_test)
-print accuracy_score(y_test,y_pred)*100
+print('Decision Tree accuracy: %.2f' % (accuracy_score(y_test,y_pred)*100))
+
+
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print('Random Forest accuracy: %.2f' % (accuracy_score(y_test,y_pred)*100))
--- regression.py
+++ regression.py
@@ -1,7 +1,6 @@
 import os
 import pymysql.cursors
 from pprint import pprint
-import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import train_test_split
@@ -9,6 +8,7 @@ import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -23,19 +23,33 @@ user_id = 'ELcQDlf69kb-ihJfxZyL0A'
 
 try:
     with connection.cursor() as cursor:
-        sql = "select b.id b_id, b.neighborhood b_hood, b.name b_name, b.address b_address, b.city b_city, b.state b_state, b.postal_code b_pc, b.latitude b_lat, b.longitude b_longi, b.stars b_stars, b.review_count b_review_count, r.stars r_stars from business b join review r on r.business_id = b.id where r.user_id = %s"
+        sql = "select c.category c_category, b.id b_id, b.neighborhood b_hood, b.name b_name, b.address b_address, b.city b_city, b.state b_state, b.postal_code b_pc, b.latitude b_lat, b.longitude b_longi, b.stars b_stars, b.review_count b_review_count, r.stars r_stars from business b join review r on r.business_id = b.id join category c on c.business_id = b.id where r.user_id = %s"
         cursor.execute(sql, [user_id])
         reviews = cursor.fetchall()
 finally:
     connection.close()
 
+y = []
+X = []
+for review in reviews:
+    row = {
+        'b_review_count': review['b_review_count'],
+        'b_hood': review['b_hood'],
+        'b_name': review['b_name'],
+        'b_address': review['b_address'],
+        'b_city': review['b_city'],
+        'b_state': review['b_state'],
+        'b_stars': review['b_stars'],
+        # 'b_postal_code': review['b_pc'],
+        'c_category': review['c_category'],
+    }
+    X.append(row)
+    y.append(review['r_stars'])
+
 vec = DictVectorizer()
-vectorized_features = vec.fit_transform(reviews).toarray()
+vectorized_features = vec.fit_transform(X).toarray()
 feature_names = vec.get_feature_names()
 
-df1 = pd.DataFrame(reviews)
-
-y = df1['r_stars']
 X_train, X_test, y_train, y_test = train_test_split(vectorized_features, y, test_size=0.5, random_state=42)
 
 # Create linear regression object
@@ -45,12 +59,13 @@ regr = linear_model.LinearRegression()
 regr.fit(X_train, y_train)
 
 # Make predictions using the testing set
-y_pred = regr.predict(X_test)
+# Rounding to the nearest integer
+y_pred = np.rint(regr.predict(X_test))
 
-# The coefficients
-print('Coefficients: \n', regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f"
       % mean_squared_error(y_test, y_pred))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(y_test, y_pred))
+# Accuracy
+print('Accuracy score: %.2f' % (accuracy_score(y_test,y_pred)*100))
